ff9cc2be85e5d5344fe830278a7ec86a513d6de3
ryo117n <ryonakagawa117@gmail.com>
2018-06-24 17:10:14 +0900

    editMemo

--- DotMemoApp/MemoTableViewController.swift
+++ DotMemoApp/MemoTableViewController.swift
@@ -64,17 +64,16 @@ class MemoTableViewController: UITableViewController {
     }
     */
 
-    /*
+
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            self.memos.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
+
 
     /*
     // Override to support rearranging the table view.
