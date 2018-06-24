6834ae0d82c1c0960ed266cacbd10cda6b344c7c
ryo117n <ryonakagawa117@gmail.com>
2018-06-24 16:53:02 +0900

    initial commit

---
+++ DotMemoApp/MemoViewController.swift
@@ -0,0 +1,57 @@
+//
+//  MemoViewController.swift
+//  DotMemoApp
+//
+//  Created by Ryo Nakagawa on 2018/06/24.
+//  Copyright © 2018年 Ryo Nakagawa. All rights reserved.
+//
+
+import UIKit
+
+class MemoViewController: UIViewController {
+    var memo: String?
+    
+    @IBOutlet weak var memoTextField: UITextField!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.saveButton.isEnabled = false
+    }
+    
+    @IBAction func memoTextFieldChanged(_ sender: Any) {
+        let memo = self.memoTextField.text ?? ""
+        self.saveButton.isEnabled = !memo.isEmpty
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let button = sender as? UIBarButtonItem, button === self.saveButton else {
+            return
+        }
+        self.memo = self.memoTextField.text ?? ""
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
